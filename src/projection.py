fdb5711be997548629a9a7f8a7b719aac1f142fd
CasperU <casper.uiterwijk@gmail.com>
2019-10-09 14:28:49 +0100

    Add function for retrieving the monitor size

--- src/projection.py
+++ src/projection.py
@@ -1,4 +1,5 @@
 import cv2
+from screeninfo import get_monitors
 
 fullscreen_window_name = "Fullscreen output"
 
@@ -6,3 +7,7 @@ def show_image_fullscreen(image):
     cv2.namedWindow(fullscreen_window_name, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_EXPANDED)
     cv2.setWindowProperty(fullscreen_window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.imshow(fullscreen_window_name, image)
+
+def get_screen_size():
+    monitors = get_monitors()
+    return (monitors[0].width, monitors[0].height)
