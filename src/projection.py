143d59915a5a4a0a0af33c13f78d1380ca6eb861
CasperU <casper.uiterwijk@gmail.com>
2019-10-23 12:01:08 +0200

    Take PROJECTION_OFFSET into account when generating projection image

--- src/projection.py
+++ src/projection.py
@@ -18,20 +18,25 @@ def show_image_fullscreen(image):
 def get_block_screen_coordinate(layer, block_pos):
     baseplate_real_size = (
         BASEPLATE_SIZE[0] * BLOCK_UNIT_LENGTH, BASEPLATE_SIZE[1] * BLOCK_UNIT_LENGTH)
-    screen_offset_x = (PROJECTOR_RESOLUTION[0] /
-                       2) * (baseplate_real_size[0] / PROJECTION_SIZE[0]) \
-        / ((CAMERA_HEIGHT - layer * BLOCK_HEIGHT) / CAMERA_HEIGHT)
-    screen_offset_y = (PROJECTOR_RESOLUTION[1] /
-                       2) * (baseplate_real_size[1] / PROJECTION_SIZE[1]) \
-        / ((CAMERA_HEIGHT - layer * BLOCK_HEIGHT) / CAMERA_HEIGHT)
+    layer_factor = 1 / ((CAMERA_HEIGHT - layer * BLOCK_HEIGHT) / CAMERA_HEIGHT)
+    # Baseplate corner screen distance from the center
+    screen_dist_x = (PROJECTOR_RESOLUTION[0] /
+                       2) * (baseplate_real_size[0] / PROJECTION_SIZE[0]) * layer_factor
+    screen_dist_y = (PROJECTOR_RESOLUTION[1] /
+                       2) * (baseplate_real_size[1] / PROJECTION_SIZE[1]) * layer_factor
+    # Projection offset in screen coordinates
+    screen_offset_x = (PROJECTOR_RESOLUTION[0] / PROJECTION_SIZE[0]) * PROJECTION_OFFSET[0]
+    screen_offset_y = (PROJECTOR_RESOLUTION[1] / PROJECTION_SIZE[1]) * PROJECTION_OFFSET[1]
+    # Generate screen coordinates for baseplate corners
     baseplate_layer_top_left_screen_coords = (
-        (PROJECTOR_RESOLUTION[0] / 2) - screen_offset_x,
-        (PROJECTOR_RESOLUTION[1] / 2) - screen_offset_y
+        (PROJECTOR_RESOLUTION[0] / 2) - screen_dist_x + screen_offset_x,
+        (PROJECTOR_RESOLUTION[1] / 2) - screen_dist_y + screen_offset_y
     )
     baseplate_layer_right_bottom_screen_coords = (
-        (PROJECTOR_RESOLUTION[0] / 2) + screen_offset_x,
-        (PROJECTOR_RESOLUTION[1] / 2) + screen_offset_y
+        (PROJECTOR_RESOLUTION[0] / 2) + screen_dist_x + screen_offset_x,
+        (PROJECTOR_RESOLUTION[1] / 2) + screen_dist_y + screen_offset_y
     )
+    # Map block position to baseplate screen corner coordinates
     screen_x = util.map_number(block_pos[0], (0, BASEPLATE_SIZE[0] - 1),
                                (baseplate_layer_top_left_screen_coords[0], baseplate_layer_right_bottom_screen_coords[0]))
     screen_y = util.map_number(block_pos[1], (0, BASEPLATE_SIZE[1] - 1),
@@ -46,9 +51,6 @@ def create_block_outline_image(layer, block_pos, block_size):
     left_top_corner = get_block_screen_coordinate(layer, block_pos)
     bottom_right_corner = get_block_screen_coordinate(
         layer, (block_pos[0] + block_size[0], block_pos[1] + block_size[1]))
-
-    print(left_top_corner)
-    print(bottom_right_corner)
 
     rectangle_thickness = 3 / \
         ((CAMERA_HEIGHT - layer * BLOCK_HEIGHT) / CAMERA_HEIGHT)
