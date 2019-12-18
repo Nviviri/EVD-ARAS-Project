ecc99f729ae21160ad406a00ee2211c0a62239da
Andreas <andreasstratakis@gmail.com>
2019-12-18 14:01:37 +0100

    Camera buffer check, return check, removed calibration

--- cpparas/include/Camera.hpp
+++ cpparas/include/Camera.hpp
@@ -14,16 +14,15 @@ public:
     ~Camera();
     void Camera_thread_worker();
     void Camera_thread_worker_start();
-    void Camera_calibrate();
+    void Camera_thread_worker_stop();
     image_t* Camera_get_frame();
 
-private:
+protected:
     //camera stuff
     std::string raspi_parameters;
     uint32_t width, height;
-    //buffers
+    //buffer
     image_t* captured_frame;
-    char *raspivid_out;
     //threads
     bool threadRunning = false;
     std::thread camera_thread; 
