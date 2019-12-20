da6fffb77c62a0627ecaff3321499c679fd19ba5
Andreas <andreasstratakis@gmail.com>
2019-12-20 15:28:13 +0100

    Initial image loader component

--- cpparas/include/ImageLoader.hpp
+++ cpparas/include/ImageLoader.hpp
@@ -1,14 +1,28 @@
 #ifndef IMAGELOADER_HPP
 #define IMAGELOADER_HPP
 
-#include "Camera.hpp"
+#include <string>
+#include "util/ImageUtils.hpp"
+#include "operators.h"
+
+enum SourceType {
+    CAMERA,
+    IMAGE,
+    DUNNO,
+};
 
 class ImageLoader {
 public:
     ImageLoader();
+    ~ImageLoader();
+    void Set_source_type(SourceType source);
+    SourceType Get_source_type();
+    void Set_source_image(std::string filePath);
+    image_t* Get_source_image();
 
 private:
-    std::shared_ptr<Camera> camera;
+    SourceType selected_source;
+    image_t* user_image;
 };
 
 #endif /* IMAGELOADER_HPP */
--- cpparas/src/ImageLoader.cpp
+++ cpparas/src/ImageLoader.cpp
@@ -1,6 +1,45 @@
 #include "ImageLoader.hpp"
 
 ImageLoader::ImageLoader()
-    : camera()
 {
+}
+
+ImageLoader::~ImageLoader()
+{
+}
+
+void ImageLoader::Set_source_type(SourceType source)
+{
+    switch(source)
+    {
+        case CAMERA:
+            selected_source = CAMERA;
+            break;
+        case IMAGE:
+            selected_source = IMAGE;
+            break;
+        default:
+            selected_source = DUNNO;
+            break;
+    };
+}
+
+SourceType ImageLoader::Get_source_type()
+{
+    return selected_source;
+}
+
+void ImageLoader::Set_source_image(std::string filePath)
+{
+    if(selected_source == IMAGE){
+        user_image = ImageUtils::loadImageFromFile(filePath);
+    }
+}
+
+image_t* ImageLoader::Get_source_image()
+{
+    if(selected_source == IMAGE){
+        return user_image;
+    }
+    return nullptr;
 }
