95e3468899ce2f555f0d5e927a3abb1fd6d754c0
Andreas <andreasstratakis@gmail.com>
2020-01-10 00:23:18 +0100

    Locator with region extractor + location checker added

--- cpparas/include/Locator.hpp
+++ cpparas/include/Locator.hpp
@@ -19,16 +19,25 @@ public:
     void Locator_thread();
     image_t* Get_new_frame();
     void Send_frame_to_ui(image_t* frame);
+    bool First_frame_received();
+    bool Location_checker();
 
 private:
     std::atomic<bool> locator_running;
     std::atomic<bool> locator_thread_starting;
+    std::atomic<bool> first_frame;
+    std::atomic<bool> moved_interupt;
     std::thread locator_thread;
     image_t* new_cut_frame;
     image_t* new_full_frame;
     SourceType source_type;
     std::shared_ptr<ImageLoader> imageLoader;
     Camera PiCamera;
+    RegionExtractor RegExtractor;
+    int32_t MAX_DIVIATION = 20;
+    std::vector<Point<int32_t>> corner_points;
+    Point<int32_t> Central_camera_point;
+    Point<int32_t> Central_board_point;
 };
 
 } // namespace cpparas
--- cpparas/src/Locator.cpp
+++ cpparas/src/Locator.cpp
@@ -10,8 +10,11 @@ namespace cpparas {
 Locator::Locator(std::shared_ptr<ImageLoader> imageLoader_)
     : locator_running(false)
     , locator_thread_starting(true)
+    , first_frame(false)
+    , moved_interupt(false)
     , imageLoader(imageLoader_)
     , PiCamera(1440, 1440)
+    , RegExtractor(800,800)
 {
 }
 
@@ -53,6 +56,8 @@ void Locator::Locator_thread()
         //start Camera thread
         PiCamera.Camera_thread_worker_start();
     }
+    image_t* old_cut_frame = newRGB888Image(800,800);
+
     while (locator_running) {
         if (source_type == CAMERA) {
             //Get a new frame from camera
@@ -71,16 +76,55 @@ void Locator::Locator_thread()
             break;
         }
 
-        //find corners, warp and crop image here
+        //Get cameras center point
+        Central_camera_point.col = new_full_frame->cols / 2;
+        Central_camera_point.row = new_full_frame->rows / 2;
 
-        //save last frame to be used by main thread
-        new_cut_frame = new_full_frame;
+        //find corners
+        corner_points = RegExtractor.updateImage(new_full_frame);
+        if(corner_points.size() == 3){
+            
+            //caclulate center point based on 3 points
+            Central_board_point.col = (corner_points[0].col + corner_points[2].col) / 2;
+            Central_board_point.row = (corner_points[0].row + corner_points[2].row) / 2;
+            
+            //compare those points with the camera central point
+            if((Central_board_point.col - Central_camera_point.col) > MAX_DIVIATION || (Central_board_point.col - Central_camera_point.col) < -MAX_DIVIATION){
+                moved_interupt = true;
+            }
+                    
+            if((Central_board_point.row - Central_camera_point.row) > MAX_DIVIATION || (Central_board_point.row - Central_camera_point.row) < -MAX_DIVIATION){
+                moved_interupt = true;
+            }
+
+            // Get the new cut frame
+            new_cut_frame = RegExtractor.getRegionImage();
+
+            // Set first frame accuaried bool
+            if(!first_frame){
+                first_frame = true;
+            }
+
+            // Save a frame copy
+            old_cut_frame = new_cut_frame;
+        } 
+        else
+        {
+            if(old_cut_frame->data != nullptr){
+                //No points found but buffer has older, saved image, so use that instead
+                new_cut_frame = old_cut_frame;
+            }
+            // If no picture was found at all, do nothing and try again
+        }
 
         //only unlock the main thread after at least a full frame has been received.
-        locator_thread_starting = false;
+        if(locator_thread_starting){
+            locator_thread_starting = false;
+        }
         //wait a bit, no need to run this at full powaa
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+    deleteImage(old_cut_frame);
     PiCamera.Camera_thread_worker_stop();
 }
 
@@ -103,6 +147,18 @@ void Locator::Send_frame_to_ui(image_t* frame)
         throw std::runtime_error("Camera and locator thread stopped unexpectedly");
     }
     imageLoader->Set_UI_frame(frame);
+}
+
+// Check if at least one frame is available
+bool Locator::First_frame_received()
+{
+    return first_frame;
+}
+
+// Check if at least one frame is available
+bool Locator::Location_checker()
+{
+    return moved_interupt;
 }
 
 } // namespace cpparas
