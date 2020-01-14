0035e8fa30bb5cdb41fd2f0a36ef44121f8dfd3c
nviviri <nick.vivirito@protonmail.com>
2020-01-14 11:14:21 +0100

    Improving stud checker

--- cpparas/include/StudChecker.hpp
+++ cpparas/include/StudChecker.hpp
@@ -11,6 +11,7 @@ namespace cpparas {
 namespace StudChecker {
 
     bool matches(const image_t* image, const CoordinateMatrix& coordinateMatrix, const std::vector<Point<uint32_t>> studCoordinates, uint32_t layer, Color expectedColor);
+    bool studMatch(const image_t* image, const CoordinateMatrix& coordinateMatrix, const Point<uint32_t> studCoordinates, uint32_t layer, Color expectedColor);
 
 }
 
--- cpparas/src/StudChecker.cpp
+++ cpparas/src/StudChecker.cpp
@@ -6,33 +6,32 @@ namespace StudChecker {
 
     bool matches(const image_t* image, const CoordinateMatrix& coordinateMatrix, const std::vector<Point<uint32_t>> studCoordinates, uint32_t layer, Color expectedColor)
     {
-        //calculate distance between studs
+        //get coordinates in image
+        Point<uint32_t> pixelCoordinates;
+        for (uint32_t i = 0; i < sizeof(studCoordinates); i++) {
+            pixelCoordinates = studCoordinates[i];
+            if(!studMatch(image, coordinateMatrix, pixelCoordinates, layer, expectedColor)){
+                return false;
+            }
+        }
+        return true;
+    }
 
-        int halfDistance = 15;
-        Point<int32_t> pt = coordinateMatrix.getMatrix()[0][0][0];
+    bool studMatch(const image_t* image, const CoordinateMatrix& coordinateMatrix, const Point<uint32_t> studCoordinates, uint32_t layer, Color expectedColor){
+        //calculate distance between studs
+        int halfDistance = (coordinateMatrix.getMatrix()[layer][0][1].col - coordinateMatrix.getMatrix()[layer][0][0].col) / 2;
         long int sum_h = 0;
         long int sum_s = 0;
         long int sum_v = 0;
         int num_pixels = 0;
+        hsv_pixel_t max = COLOR_HSV_MAX.at(expectedColor);
+        hsv_pixel_t min = COLOR_HSV_MIN.at(expectedColor);
         hsv_pixel_t pixelHSV;
         hsv_pixel_t average;
-        hsv_pixel_t max = { 255, 255, 255 };
-        hsv_pixel_t min = { 0, 0, 0 };
-        (void)coordinateMatrix;
-        (void)studCoordinates;
-        (void)expectedColor;
-
-        //get coordinates in image
-        Point<int32_t> pixelCoordinates;
-        for (uint32_t i = 0; i < sizeof(studCoordinates); i++) {
-        }
-        pixelCoordinates.col = 500;
-        pixelCoordinates.row = 500;
-
         //Calculate average HSV around stud
         for (int r = -halfDistance; r < halfDistance; r++) {
             for (int c = -halfDistance; c < halfDistance; c++) {
-                pixelHSV = getHSVPixel(image, c + pixelCoordinates.col, r + pixelCoordinates.row);
+                pixelHSV = getHSVPixel(image, c + studCoordinates.col, r + studCoordinates.row);
                 sum_h = sum_h + pixelHSV.h;
                 sum_s = sum_s + pixelHSV.s;
                 sum_v = sum_v + pixelHSV.v;
