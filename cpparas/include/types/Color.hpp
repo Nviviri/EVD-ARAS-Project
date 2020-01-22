c5efbc1a83a892392b7327f8376683ca9ab980f2
CasperU <casper.uiterwijk@gmail.com>
2020-01-22 12:23:44 +0100

    Move calibration bricks

--- cpparas/include/types/Color.hpp
+++ cpparas/include/types/Color.hpp
@@ -1,8 +1,8 @@
 #ifndef COLOR_HPP
 #define COLOR_HPP
 
-#include "operators.h"
 #include "Point.hpp"
+#include "operators.h"
 #include <map>
 
 namespace cpparas {
@@ -33,19 +33,19 @@ const std::map<Color, rgb888_pixel_t> COLOR_DISPLAY_VALUES = {
 };
 
 const std::map<Color, Point<int32_t>> COLOR_COORDINATES_BOTTOM = {
-    { Color::ORANGE, { 36, 44 } },
-    { Color::GREEN, { 34, 44 } },
-    { Color::YELLOW, { 32,44 } },
-    { Color::BLUE, { 30, 44 } },
-    { Color::RED, {28, 44} }
+    { Color::ORANGE, { 12, 18 } },
+    { Color::GREEN, { 12, 20 } },
+    { Color::YELLOW, { 12, 22 } },
+    { Color::BLUE, { 12, 24 } },
+    { Color::RED, { 12, 26 } }
 };
 
 const std::map<Color, Point<int32_t>> COLOR_COORDINATES_TOP = {
-    { Color::ORANGE, { 10, 3 } },
-    { Color::GREEN, { 12, 3 } },
-    { Color::YELLOW, { 14, 3 } },
-    { Color::BLUE, { 16, 3 } },
-    { Color::RED, {18, 3} }
+    { Color::ORANGE, { 34, 26 } },
+    { Color::GREEN, { 34, 24 } },
+    { Color::YELLOW, { 34, 22 } },
+    { Color::BLUE, { 34, 20 } },
+    { Color::RED, { 34, 18 } }
 };
 
 } // namespace cpparas
